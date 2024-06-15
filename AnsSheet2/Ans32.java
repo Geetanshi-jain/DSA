596c5d03379f9cf2717bb434718809ac2598e914
Geetanshi jain <jaingeetanshi175@gmail.com>
2024-06-15 20:47:07 +0530

    array sheets

--- AnsSheet2/Ans32.java
+++
@@ -1,9 +0,0 @@
-public class Ans32 {
-    // . Write a Java program to check if an array of integers contains two
-    // specified
-    // elements 65 and 77.
-    public static void main(String[] args) {
-        
-    }
-
-}
